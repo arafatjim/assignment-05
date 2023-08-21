e456d845b5e7c7d036830a8d40252043dcbbd616
arafatjim <jimyeasirarafat28@gmail.com>
2023-08-21 23:20:45 +0600

    third commit

---
+++ JS/index.js
@@ -0,0 +1,61 @@
+// document.getElementById('cards').addEventListener('click',function(event){
+//           const cardName=document.getElementById('card-name');
+//           const cardNameString=cardName.innerText;
+          
+//           const li=document.createElement("li");
+//           li.innerText=cardNameString;
+//           console.log(li);
+          
+          
+// })
+let total=0;
+let coupon= "SELL200";
+function cardClickBtn(target){
+          const selectedItemContainer=document.getElementById('selected-items');
+          const itemName=target.childNodes[3].innerText;
+          const li=document.createElement("li");
+           li.innerText=itemName;
+          selectedItemContainer.appendChild(li)
+          const price=target.childNodes[5].innerText.split(" ")[0];
+          //console.log(target.childNodes[5].innerText.split(" ")[0]);
+         total=parseInt(total) + parseInt(price);
+         const firstTotal=(document.getElementById('subTotal').innerText=total);
+         finalTotal=(document.getElementById('grandTotal').innerText=firstTotal);
+         const couponCode=(document.getElementById('couponInput').value);
+
+         document.getElementById('coupon').addEventListener('click',function(){
+          const couponCode=document.getElementById('couponInput').value;
+          
+          if( couponCode == "SELL200" && firstTotal >= 200){
+                    const discount= firstTotal * 0.2;
+                    const newTotal=firstTotal - discount;
+                    const discountPrice=(document.getElementById('discountPrice').innerText=discount);
+                    console.log(discountPrice);
+                    const finalTotal=(document.getElementById('grandTotal').innerText=newTotal);
+          }
+
+                    
+          
+         })
+         
+         
+//          const firstTotal=(document.getElementById('subTotal').innerText=total);
+//          const grandTotal=firstTotal;
+//           const grandTotal(document.getElementById('grandTotal').innerText);
+//          document.getElementById('coupon').addEventListener('click',function(){
+//           const couponCode=document.getElementById('couponInput');
+          
+                    
+          //           //grandTotal=(document.getElementById('grandTotal').innerText=grandTotal);
+          //          discount=(document.getElementById('discountPrice').innerText=discount);
+          //          const grandDiscountTotal= total - discount;
+          //          grandDiscountTotal=(document.getElementById('grandTotal').innerText=grandDiscountTotal);
+                   
+          //          }
+         //}
+         
+         
+         
+         
+         
+}
