5f59a2bc55837f6e67ab8e1c0f3fb9d6394a5512
arafatjim <jimyeasirarafat28@gmail.com>
2023-08-23 13:35:41 +0600

    btn disable & enable set

--- JS/index.js
+++ JS/index.js
@@ -2,30 +2,31 @@
 let total=0;
 const coupon= "SELL200";
 const couponBtn=document.getElementById('coupon');
-
-          
+const parcheseBtn=(document.getElementById('btnParchese').disabled=true);
+couponBtn.disabled=true;
 
 function cardClickBtn(target){
-          
+         
           const selectedItemContainer=document.getElementById('selected-items');
+          const parcheseBtn=(document.getElementById('btnParchese').disabled=false);
           const itemName=target.childNodes[3].innerText;
           const li=document.createElement("li");
            li.innerText=itemName;
           selectedItemContainer.appendChild(li)
+          
           const price=target.childNodes[5].innerText.split(" ")[0];
-          //console.log(target.childNodes[5].innerText.split(" ")[0]);
          total=parseInt(total) + parseInt(price);
          const firstTotal=(document.getElementById('subTotal').innerText=total);
          finalTotal=(document.getElementById('grandTotal').innerText=firstTotal);
+         
          const couponCode=(document.getElementById('couponInput').value);
          
           if(total >= 200){
           couponBtn.disabled=false;
           
           }
-          else{
-                    couponBtn.disabled=true;
-          }
+          
+          
          document.getElementById('coupon').addEventListener('click',function(){
           const couponCode=document.getElementById('couponInput').value;
 
@@ -40,10 +41,12 @@ function cardClickBtn(target){
                     const newTotal=firstTotal - discount;
                     const discountPrice=(document.getElementById('discountPrice').innerText=discount);
                     console.log(discountPrice);
+                    discountPrice.toFixed(4);
+                    
                     const finalTotal=(document.getElementById('grandTotal').innerText=newTotal);
           }
           
-                    
+          
           
          });
          
